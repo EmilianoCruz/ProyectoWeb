dfdaf8970a70263989ec192502b50fcf891923fd
EmilianoCruz <emilianocruz01@aragon.unam.mx>
2023-11-27 17:31:48 -0600

    Index terminado

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href ="estilos.css">
     <title>Proyecto final</title>
+    
 </head>
 <body>
-    
+    <div class='encabezados'>
+        <h1>Proyecto final Programación web I 1510</h1>
+        <p>Emiliano Cruz</p>
+    </div>
+    <div class = 'menu'>
+        <a href="registro.php"> Registro</a>
+        <a href="login.php">Inciar Sesión</a>
+    </div>
 </body>
 </html>
